d00d612c22941a30cc25a4b0dde188776a651ca0
mrxie68 <mrxie6@gmail.com>
2023-07-10 00:42:42 +0800

    Update

---
+++ state/config.js
@@ -0,0 +1,35 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Public Status',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm794552326-9bdc561a6ddc1418d9c1b80c',
+    'm786847877-54a6e63fa5a0f61e3d617422',
+    'm794758305-26f0f73270904dd8baedf38f',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: false,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://status.org.cn/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/yb/uptime-status'
+    },
+    {
+      text: 'Blog',
+      url: 'https://abo.xyz/'
+    },
+  ],
+};
